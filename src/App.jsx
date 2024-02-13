40baa1d8bea4adf841dcc231264c580a1e164b09
beloyosry <105988333+beloyosry@users.noreply.github.com>
2024-02-13 03:53:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Header from "./components/ui/Header/Header.jsx";
 import HomePhone from "./pages/Home system/Home phone/HomePhone";
 import Calculator from "./pages/calculator/Calculator.jsx";
 import HomeBar from "./components/ui/HomeBar/HomeBar.jsx";
+import AppStore from "./pages/AppStore/AppStore.jsx";
 
-function App() {
+function App ()
+{
   return (
-    <div className=" relative rounded-[50px] overflow-hidden p-3 border-[3px] border-gray-500 bg-black h-[95vh] w-[45vh]">
+    <div className=" relative rounded-[50px] overflow-hidden p-3 border-[3px] border-gray-500 bg-black h-screen w-screen">
       <Routes>
         <Route
           path="/"
@@ -27,13 +29,13 @@ function App() {
             </>
           }
         />
-         <Route 
+        <Route
           path="/appstore"
           element={
             <>
-              <Header/>
-              <AppStore/>
-              <HomeBar/>
+              <Header />
+              <AppStore />
+              <HomeBar />
             </>
           }
         />
